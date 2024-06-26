5d81b1a158aa20612770e29015dcb117c1c0e29b
Christian Carvalho <christiangracecarvalho@gmail.com>
2024-06-26 15:12:53 -0300

    Teste com react

---
+++ react-start/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-slate-800'>
+       <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 className='bg-slate-200'>React Tailwind CSS</h1>
+    </div>
+  )
+}
+
+export default App
